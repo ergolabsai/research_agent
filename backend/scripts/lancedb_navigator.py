ae02b4b221cf46b819950e6f2a0e6519d4c40354
JR Barhydt <jrbarhydt@gmail.com>
2026-03-10 10:59:02 -0700

    update lancedb navigator helper text

--- backend/scripts/lancedb_navigator.py
+++ backend/scripts/lancedb_navigator.py
@@ -1,6 +1,6 @@
 """
 LanceDB Navigator - A Streamlit app for exploring and searching LanceDB tables
-streamlit run backend/scripts/sandbox.py
+streamlit run backend/scripts/lancedb_navigator.py
 """
 import streamlit as st
 import lancedb
